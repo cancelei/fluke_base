9ed27e4ddbdea8473ea2171baa4ff6d5ec055ea2
cancelei <glauber.bannwart@gmail.com>
2025-04-30 12:14:13 -0300

    feat/update dockerfile

---
+++ app/javascript/channels/chat_channel.js
@@ -0,0 +1,21 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create({ channel: "ChatChannel", room: "general" }, {
+  connected() {
+    console.log("Connected to chat channel")
+  },
+
+  disconnected() {
+    console.log("Disconnected from chat channel")
+  },
+
+  received(data) {
+    const messages = document.getElementById('messages')
+    const messageElement = document.createElement('div')
+    const userSpan = document.createElement('strong')
+    userSpan.textContent = `${data.user}: `
+    messageElement.appendChild(userSpan)
+    messageElement.appendChild(document.createTextNode(data.message))
+    messages.appendChild(messageElement)
+  }
+}) 
