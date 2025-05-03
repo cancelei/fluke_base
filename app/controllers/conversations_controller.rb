a1a0109b1487538da118ff796d6ee5ca53210bb3
cancelei <glauber.bannwart@gmail.com>
2025-05-03 11:02:31 -0300

    feat/add improvement in message exchange service.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,13 +3,17 @@ class ConversationsController < ApplicationController
   before_action :set_conversation, only: [ :show ]
 
   def index
-    @conversations = Conversation.involving(current_user).includes(:sender, :recipient, messages: :user)
+    @conversations = Conversation.involving(current_user)
+                                .includes(:sender, :recipient, messages: :user)
+                                .order_by_most_recent_message
     @conversation = @conversations.first if @conversations.any?
     @message = Message.new if @conversation
   end
 
   def show
-    @conversations = Conversation.involving(current_user).includes(:sender, :recipient, messages: :user)
+    @conversations = Conversation.involving(current_user)
+                                .includes(:sender, :recipient, messages: :user)
+                                .order_by_most_recent_message
     @messages = @conversation.messages.to_a
     @message = Message.new
 
@@ -18,7 +22,9 @@ class ConversationsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.turbo_stream
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("conversation_content", partial: "conversations/conversation_content", locals: { conversation: @conversation, messages: @messages, message: @message })
+      end
     end
   end
 
