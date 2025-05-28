4fcef60011c2cd890494b37ee803cb6dae1d60e6
Sheraz <sherazp995@gmail.com>
2025-05-28 23:16:16 +0500

    Some code fixes.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -21,6 +21,6 @@ class ProfileController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :avatar, :bio, :industry, expertise: [])
   end
 end
