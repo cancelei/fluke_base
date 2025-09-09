fffb10da31d4a6eef415bded04ba33d5abd82435
cancelei <glauber.bannwart@gmail.com>
2025-09-09 17:26:47 -0300

    feat/fix project index 3-dot menu visibility

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,14 +3,38 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
+  connect() {
+    this.boundHide = this.hide.bind(this)
+  }
+
   toggle(event) {
     event.preventDefault()
-    this.menuTarget.classList.toggle("hidden")
+    event.stopPropagation()
+    
+    if (this.menuTarget.classList.contains("hidden")) {
+      this.show()
+    } else {
+      this.hideMenu()
+    }
+  }
+
+  show() {
+    this.menuTarget.classList.remove("hidden")
+    document.addEventListener("click", this.boundHide)
+  }
+
+  hideMenu() {
+    this.menuTarget.classList.add("hidden")
+    document.removeEventListener("click", this.boundHide)
   }
 
   hide(event) {
     if (!this.element.contains(event.target)) {
-      this.menuTarget.classList.add("hidden")
+      this.hideMenu()
     }
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.boundHide)
   }
 } 
