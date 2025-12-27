12cb52aea4a9ef17c59082505d80c642c7386eeb
cancelei <glauber.bannwart@gmail.com>
2025-12-27 00:58:51 -0300

    update enc

--- config/initializers/active_record_encryption.rb
+++ config/initializers/active_record_encryption.rb
@@ -8,9 +8,20 @@
 #   - ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY
 #   - ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY
 #   - ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT
+#
+# NOTE: We must call ActiveRecord::Encryption.configure() explicitly to avoid
+# a timing issue in eager loading mode where models load before after_initialize runs.
+# See: https://github.com/rails/rails/issues/50604
 
 if ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"].present?
   Rails.application.config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
   Rails.application.config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
   Rails.application.config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+
+  # Force immediate configuration to ensure encryption is ready before models load
+  ActiveRecord::Encryption.configure(
+    primary_key: ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"],
+    deterministic_key: ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"],
+    key_derivation_salt: ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+  )
 end
